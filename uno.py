68cd9d49b5172c1254c42462536221019d0eda11
virajrathod <virajrathod99@gmail.com>
2019-12-13 15:42:23 -0700

    First commit

---
+++ uno.py
@@ -0,0 +1,109 @@
+# # 1
+# print("Hello")
+# hi = "Hi"
+# num = hi;
+# print(num)
+
+# 2
+# nums=[]
+# nums.append(21)
+# nums.append(3022.11)
+# nums.append("Hi Mera Naam Baburao hai")
+# print(nums)
+
+# 3
+# grade = int(input("What are your marks out of 100? "))
+#
+# if (grade > 93 ):
+#     print("Your grade is A for " + str(grade) + " marks")
+# elif (grade < 93):
+#     print("Your grade is A- for " + str(grade) + " marks")
+# elif (grade < 90):
+#     print("Your grade is B+ for " + str(grade) + " marks")
+# else:
+#     print("Your grade is B- for " + str(grade) + " marks")
+
+# 4
+# name = input("enter a name: ")
+# print("Hello", "My name is", name)
+
+# 5
+# def hello():
+#     num = 10
+#     if(num > 0): {
+#         print("hi")
+#     }
+#
+#
+#     print("Just something")
+#
+#     return 1
+#
+# hello()
+
+# 6
+# def getInteger():
+#     result = int(input("Enter an intger:"))
+#     return result
+#
+# def Main():
+#     print("Started")
+#     output = getInteger()
+#     print(output)
+
+# now we are required to tell Python
+# for 'Main' function existence
+# if __name__ == "__main__":
+#     Main()
+
+
+################################## HACKER RANK ######################################
+
+
+# 6
+# i = 4
+# d = 4.0
+# s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+# i1 = int(input(12))
+# d1 = float(input(4.0))
+# s1 = input('is the best place to learn and practice coding!')
+
+# Print the sum of both integer variables on a new line.
+# sumi = i + i1
+# print(sumi)
+# Print the sum of the double variables on a new line.
+# sumd = d + d1
+# print(sumd)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+# print(s + s1)
+
+# 7
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+# def solve(meal_cost, tip_percent, tax_percent):
+#     tip_percent = meal_cost * (tip_percent/100)
+#     tax_percent = meal_cost * (tax_percent/100)
+#     totalCost = meal_cost + tip_percent + tax_percent
+#     print(round(totalCost))
+#
+# if __name__ == '__main__':
+#     meal_cost = float(input('Enter the meal cost: '))
+#
+#     tip_percent = int(input('Enter the tip: '))
+#
+#     tax_percent = int(input('How much tax did you pay? '))
+#
+#     solve(meal_cost, tip_percent, tax_percent)
+
+# 8
